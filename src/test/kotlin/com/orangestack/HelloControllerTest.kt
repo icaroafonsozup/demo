39ce7791b132c4f5902e4dbca73016df9efd2937
elias.alves@zup.com.br <elias.alves@zup.com.br>
2021-08-17 16:05:13 -0300

    Change hello

--- src/test/kotlin/com/orangestack/HelloControllerTest.kt
+++ src/test/kotlin/com/orangestack/HelloControllerTest.kt
@@ -20,6 +20,6 @@ class HelloControllerTest {
         val request: HttpRequest<Any> = HttpRequest.GET("/hello")
         val body = client.toBlocking().retrieve(request)
         assertNotNull(body)
-        assertEquals("Hello Orange", body)
+        assertEquals("Hello Orange Stack", body)
     }
 }
